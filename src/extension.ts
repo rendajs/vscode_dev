22e2b9ecfea7b13aa20aa674685c71d75c923d1a
Jesper van den Ende <jespertheend@gmail.com>
2022-09-17 21:41:18 +0200

    Register a formatter

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,37 @@
 import * as vscode from "vscode";
+import * as cp from "child_process";
+import * as path from "path";
 
 export function activate(context: vscode.ExtensionContext) {
-	console.log("Hello world :)");
+	const disposable = vscode.languages.registerDocumentFormattingEditProvider("javascript", {
+		async provideDocumentFormattingEdits(document) {
+			const cwd = path.dirname(document.fileName);
+			const resultStr = await exec(`deno task lint-fix --ide-extension-file=${document.fileName}`, {
+				cwd,
+			});
+			const result = JSON.parse(resultStr);
+			if (result.output == undefined) {
+				return [];
+			} else {
+				const firstLine = document.lineAt(0);
+				const lastLine = document.lineAt(document.lineCount - 1);
+				const fullRange = new vscode.Range(firstLine.range.start, lastLine.range.end);
+				return [vscode.TextEdit.replace(fullRange, result.output)];
+			}
+		}
+	});
+
+	context.subscriptions.push(disposable);
 }
 
-export function deactivate() {}
+function exec(cmd: string, options: cp.ExecOptions = {}) {
+	return new Promise<string>((resolve, reject) => {
+		cp.exec(cmd, options, (err, stdout, stderr) => {
+			if (err) {
+				reject(err);
+			} else {
+				resolve(stdout);
+			}
+		});
+	})
+}
